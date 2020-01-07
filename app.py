af12794dff97c9ff07c90a7fd7a78025fceea182
Hoang Linh <linhhh@bap.jp>
2020-01-07 20:27:42 +0700

    update

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-# using flask_restful 
-from flask import Flask, jsonify, request 
-from flask_restful import Resource, Api, reqparse 
+# using flask_restful
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 import os
 import aiml
 
-# creating the flask app 
-app = Flask(__name__) 
-# creating an API object 
-api = Api(app) 
+# creating the flask app
+app = Flask(__name__)
+# creating an API object
+api = Api(app)
 CORS(app, resource=r'/ask/*')
-# making a class for a particular resource 
-# the get, post methods correspond to get and post requests 
-# they are automatically mapped by flask_restful. 
-# other methods include put, delete, etc. 
-class Hello(Resource): 
-  
-    # corresponds to the GET request. 
-    # this function is called whenever there 
-    # Corresponds to POST request 
-    def post(self): 
+# making a class for a particular resource
+# the get, post methods correspond to get and post requests
+# they are automatically mapped by flask_restful.
+# other methods include put, delete, etc.
+class Hello(Resource):
+
+    # corresponds to the GET request.
+    # this function is called whenever there
+    # Corresponds to POST request
+    def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('text', type=str)
         args = parser.parse_args()
@@ -45,16 +45,21 @@ class Hello(Resource):
                 # print bot_response
                 return jsonify({'status':'OK','answer':bot_response})
 
-            # data = request.get_json()     # status code 
+            # data = request.get_json()     # status code
             # print('Text: ' + text)
             # return jsonify({'data': text})
-    
-  
-# adding the defined resources along with their corresponding urls 
-api.add_resource(Hello, '/ask/') 
-  
-  
-# driver function 
-if __name__ == '__main__': 
-  
-    app.run(debug = True) 
+
+
+class GetTest(Resource):
+        def get(self):
+            return jsonify({'status':'OK','answer': 'haha'}
+
+# adding the defined resources along with their corresponding urls
+api.add_resource(Hello, '/ask/')
+api.add_resource(GetTest, '/ask/')
+
+
+# driver function
+if __name__ == '__main__':
+
+    app.run(debug = True)
